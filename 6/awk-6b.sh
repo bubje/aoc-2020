5ad1f35080133f3853c3858399dc93870a4f0315
Jan Gyselinck <jan@gyselinck.com>
2020-12-06 15:26:14 +0100

    adding all files up to day 6b

--- 6/awk-6b.sh
+++ 6/awk-6b.sh
@@ -12,11 +12,13 @@ INPUTFILE="$1"
 gawk -v RS= -F"\n" 'BEGIN { total=0 }
 	{	
 		for (i=1;i<=length($0);i++) {
-			if(substr($0, i, 1) ~ /^[a-z]$/) array[substr($0, i, 1)]=1
+			if(substr($0, i, 1) ~ /^[a-z]$/) array[substr($0, i, 1)]+=1
 		}
-		total+=length(array)
-		for (var in array)
+		for (var in array) {
+			if(array[var] == NF)
+				total++
      			delete array[var]
+		}
 	}
     END { print total }' "$INPUTFILE"
 
